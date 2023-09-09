a373d052b07cf35f6b191bf0f8293a861fa6ddf1
f20210008@goa.bits-pilani.ac.in <f20210008@goa.bits-pilani.ac.in>
2023-09-09 05:41:37 +0000

    Edited settings, Base, Add_Projects and Admin

--- client_code/Add_Projects/__init__.py
+++ client_code/Add_Projects/__init__.py
@@ -1,12 +1,18 @@
 from ._anvil_designer import Add_ProjectsTemplate
 from anvil import *
-import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.users
+import anvil.server
+import re
+
+def go_to_admin_page(self, **event_args):
+  user = anvil.users.login_with_form()
+  if user is not None and user['email']=="admin@admin.com":
+    open_form('Admin')
 
 class Add_Projects(Add_ProjectsTemplate):
   def __init__(self, **properties):
@@ -16,4 +22,33 @@ class Add_Projects(Add_ProjectsTemplate):
     # Any code you write here will run before the form opens.
 
   def admin_view_click(self, **event_args):
-    open_form('Admin')
+    user = anvil.users.login_with_form()
+    if user is not None and user['confirmed_email']:
+      open_form('Admin')
+
+  def submit_click(self, **event_args):
+    uploaded_file = self.brief.file
+    if self.title_box.text is None or self.title_box.text == "":
+      self.markdown.content = "Please Enter a valid Title."
+    elif uploaded_file is None:
+      self.markdown.content = "Please Upload a Brief (docx)."
+    else:
+    # if self.name_box.text is not None and self.name_box.text is not None:
+      processed_data = anvil.server.call('process_project', self.title_box.text,self.instructions_box.text, uploaded_file)
+    # Add the processed data to the 'Candidates' data table
+      app_tables.projects.add_row(
+        uid=processed_data.get('uid', ''),
+        title=processed_data.get('title', ''),
+        instructions=processed_data.get('instructions', ''),
+        brief = uploaded_file,
+        skills=processed_data.get('skills', '[]'),  # Store as a JSON string
+        domains=processed_data.get('domains', '[]'),  # Store as a JSON string
+        allotted=False
+      )
+      print(processed_data)
+      self.markdown.content = "Submitted!"
+      # open_form('View_Candidates')
+
+  def resume_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    pass
