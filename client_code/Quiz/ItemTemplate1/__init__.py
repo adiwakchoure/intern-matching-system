c35a47cea747dbdea49c05e5f067cf2652825f77
f20210008@goa.bits-pilani.ac.in <f20210008@goa.bits-pilani.ac.in>
2023-09-12 05:57:56 +0000

    Created Quiz.ItemTemplate1; Edited Quiz

--- client_code/Quiz/ItemTemplate1/__init__.py
+++ client_code/Quiz/ItemTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import QuizTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,16 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Quiz(QuizTemplate):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # print(project)
-    self.question1.text=question
 
     # Any code you write here will run before the form opens.
-
-  def radio_button_1_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    passpasspass
-
