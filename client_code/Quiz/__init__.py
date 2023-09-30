4e2c74227f087081ffd0a3f3ea23d521a43fd7d9
f20210008@goa.bits-pilani.ac.in <f20210008@goa.bits-pilani.ac.in>
2023-09-30 07:06:24 +0000

    Created Result; Edited settings, theme/parameters.yaml and Quiz

--- client_code/Quiz/__init__.py
+++ client_code/Quiz/__init__.py
@@ -18,10 +18,110 @@ class Quiz(QuizTemplate):
     # print(student_id)
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.label_2.text=dict(app_tables.candidates.get(uid=self.student_id))["skills"]
-    self.label_2.text=dict(app_tables.candidates.get(uid=self.student_id))["skills"]
-    pass
+    skills = dict(app_tables.candidates.get(uid=self.student_id))["skills"]
+    pid = anvil.server.call('project_search', "skills", str(skills))
+    self.questions_data = app_tables.questions.search(pid=pid)
+
+    self.question1.text = self.questions_data[0]['question']
+    self.r11.text = self.questions_data[0]['options'][0]['option']
+    self.r12.text = self.questions_data[0]['options'][1]['option']
+    self.r13.text = self.questions_data[0]['options'][2]['option']
+    self.r14.text = self.questions_data[0]['options'][3]['option']
+
+    self.question2.text = self.questions_data[1]['question']
+    self.r21.text = self.questions_data[1]['options'][0]['option']
+    self.r22.text = self.questions_data[1]['options'][1]['option']
+    self.r23.text = self.questions_data[1]['options'][2]['option']
+    self.r24.text = self.questions_data[1]['options'][3]['option']
+
+    self.question3.text = self.questions_data[2]['question']
+    self.r31.text = self.questions_data[2]['options'][0]['option']
+    self.r32.text = self.questions_data[2]['options'][1]['option']
+    self.r33.text = self.questions_data[2]['options'][2]['option']
+    self.r34.text = self.questions_data[2]['options'][3]['option']
+
+
+  def submit_click(self, **event_args):
+    score = 0
+    # Check answer for question 1
+    if self.r11.checked == self.questions_data[0]['options'][0]['is_correct']:
+        score += 1
+    if self.r12.checked == self.questions_data[0]['options'][1]['is_correct']:
+        score += 1
+    if self.r13.checked == self.questions_data[0]['options'][2]['is_correct']:
+        score += 1
+    if self.r14.checked == self.questions_data[0]['options'][3]['is_correct']:
+        score += 1
+
+    # Check answer for question 2
+    if self.r21.checked == self.questions_data[1]['options'][0]['is_correct']:
+        score += 1
+    if self.r22.checked == self.questions_data[1]['options'][1]['is_correct']:
+        score += 1
+    if self.r23.checked == self.questions_data[1]['options'][2]['is_correct']:
+        score += 1
+    if self.r24.checked == self.questions_data[1]['options'][3]['is_correct']:
+        score += 1
+
+    # Check answer for question 3
+    if self.r31.checked == self.questions_data[2]['options'][0]['is_correct']:
+        score += 1
+    if self.r32.checked == self.questions_data[2]['options'][1]['is_correct']:
+        score += 1
+    if self.r33.checked == self.questions_data[2]['options'][2]['is_correct']:
+        score += 1
+    if self.r34.checked == self.questions_data[2]['options'][3]['is_correct']:
+        score += 1
+
+    response = []
+
+    question1_response = {
+        'question_id': self.questions_data[0]['uid'],
+        'responses': [
+            self.r11.checked,
+            self.r12.checked,
+            self.r13.checked,
+            self.r14.checked
+        ]
+    }
+    response.append(question1_response)
+
+    # Check answer for question 2
+    question2_response = {
+        'question_id': self.questions_data[1]['uid'],
+        'responses': [
+            self.r21.checked,
+            self.r22.checked,
+            self.r23.checked,
+            self.r24.checked
+        ]
+    }
+    response.append(question2_response)
+
+    # Check answer for question 3
+    question3_response = {
+        'question_id': self.questions_data[2]['uid'],
+        'responses': [
+            self.r31.checked,
+            self.r32.checked,
+            self.r33.checked,
+            self.r34.checked
+        ]
+    }
+    response.append(question3_response)
+
+    app_tables.answers.add_row(
+      student_id=self.student_id,
+      score = score,
+      response = response
+      )
+    
+    open_form('Result',score=score)
+
+  
+
+
+  
+
 
 
