dc1d81195b01a99e3cb3a2ecb7dcff2e2f888c57
f20210008@goa.bits-pilani.ac.in <f20210008@goa.bits-pilani.ac.in>
2023-10-21 04:50:13 +0000

    Created View_Projects.RowTemplate7; Edited settings, View_Projects, View_Candidates and View_Candidates.RowTemplate4

--- client_code/View_Projects/RowTemplate7/__init__.py
+++ client_code/View_Projects/RowTemplate7/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate4Template
+from ._anvil_designer import RowTemplate7Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,19 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate4(RowTemplate4Template):
+class RowTemplate7(RowTemplate7Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def resumedownload_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-
--- client_code/View_Projects/__init__.py
+++ client_code/View_Projects/__init__.py
@@ -1,3 +1,22 @@
+# from ._anvil_designer import View_CandidatesTemplate
+# from anvil import *
+# import anvil.server
+# import anvil.google.auth, anvil.google.drive
+# from anvil.google.drive import app_files
+# import anvil.users
+# import anvil.tables as tables
+# import anvil.tables.query as q
+# from anvil.tables import app_tables
+
+# class View_Candidates(View_CandidatesTemplate):
+#   def __init__(self, **properties):
+#     self.init_components(**properties)
+
+#     # Any code you write here will run before the form opens.
+
+#   def admin_view_click(self, **event_args):
+#     open_form('Admin')
+
 from ._anvil_designer import View_ProjectsTemplate
 from anvil import *
 import anvil.server
@@ -10,11 +29,12 @@ from anvil.tables import app_tables
 
 class View_Projects(View_ProjectsTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    # Fetch all records from the 'candidates' table
+    # candidates_data = app_tables.candidates.search()
+    # Set the 'items' property of the Data Grid to the fetched data
+    # self.grid.items = anvil.server.call('get_candidates')
+    self.griddata.items = anvil.server.call('get_all_projects')
 
   def admin_view_click(self, **event_args):
     open_form('Admin')
-
