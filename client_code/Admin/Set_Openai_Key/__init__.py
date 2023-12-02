6bbb0757ec5cf198ae3a67a48e8df4e78f4baf5f
f20210008@goa.bits-pilani.ac.in <f20210008@goa.bits-pilani.ac.in>
2023-12-02 06:33:57 +0000

    Edited settings, Admin.Set_Openai_Key and Admin

--- client_code/Admin/Set_Openai_Key/__init__.py
+++ client_code/Admin/Set_Openai_Key/__init__.py
@@ -13,14 +13,20 @@ class Set_Openai_Key(Set_Openai_KeyTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.key_text.text = "sk-**********"
-
     # Any code you write here will run before the form opens.
 
   def setKey_click(self, **event_args):
     """This method is called when the button is clicked"""
-    print(self.key_text.text)
-    alert((f"Set Key: {self.key_text.text}"))
-    pass
+    keyText = self.key_text.text  # Retrieve the text from key_text component
+
+    user = anvil.users.get_user()  # Get the current logged-in user
+    if user:
+        user.update(api_key=keyText)  # Update the api_key field of the user record
+        alert(f"Your key is updated!")
+    else:
+        alert("No user is currently logged in.", title="Error")
+
+
 
   def button_1_click(self, **event_args):
     open_form('Admin')
